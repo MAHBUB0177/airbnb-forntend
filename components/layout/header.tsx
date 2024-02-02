879ab9e76385bb7ff4b10ea7ead45ac804043840
mahbub alam <ma01776879@gmail.com>
2024-02-02 22:40:21 +0600

    start serach pannel design

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -19,18 +19,25 @@ const Header = () => {
 
   return (
     <>
-      <div className=" bg-primary h-[80px] top-0 w-[100%] fixed  border-b-[1px]  border-slate-200 flex flex-row justify-between items-center px-2 md:px-20 py-3">
+      <div className=" bg-primary h-[10%] top-0 w-[100%] fixed  border-b-[1px]  border-slate-200 flex flex-row justify-between items-center px-2 md:px-10 lg:px-20 md:py-10">
         <div className="flex gap-1">
           <Image src={airbnb_logo} alt="airbnb_logo" width={30} height={20} />
-          <p className="text-secondary text-2xl font-bold">airbnb</p>
+          <p className="hidden lg:block text-secondary text-2xl font-bold ">airbnb</p>
         </div>
 
-        <div className="bg-slate-300 rounded-md">search panel</div>
+        <div className="bg-primary flex justify-between items-center gap-2 border-[1px] border-slate-200  rounded-full px-3 py-3 cursor-pointer divide-x divide-slate-300 w-auto">
+            <div className=" font-medium px-2">AnyWhere </div>
+            <div  className=" font-medium px-2">check in</div>
+            <div  className=" font-medium px-2">check out</div>
+            <div  className=" text-gray-600 px-2"> add guests</div>
+            {/* hello my name is mahbul alam i am a student i want to studu in cse */}
+
+        </div>
 
         <div className="flex flex-row gap-2">
           <p className="hidden md:block pt-3 font-medium ">airbnb your home </p>
           <div
-            className="flex gap-2 border-2 border-slate-200  rounded-full p-2 cursor-pointer"
+            className="flex gap-2 border-[1px] border-slate-200  rounded-full p-2 cursor-pointer"
             onClick={() => setIsshow((prevshow) => !prevshow)}
           >
             <IoMenuSharp
