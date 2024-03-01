082f561abebd69f55be67f7f406e214011f39cfd
mahbub alam <ma01776879@gmail.com>
2024-03-01 22:34:26 +0600

    remove text

--- components/about.tsx
+++ components/about.tsx
@@ -11,7 +11,7 @@ const About = () => {
     <div>
         <div className=' flex justify-center items-center  h-[300px] w-[400px] md:px-20'>
             <button className='bg-red-400 rounded-md w-[150px]' onClick={handleButtonClick}>
-                    button about
+                    button 
             </button>
 
         </div>
