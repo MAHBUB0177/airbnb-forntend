0412b99d6147d17cb16660a5b28ab828614f939b
mahbub alam <ma01776879@gmail.com>
2024-04-18 23:08:43 +0600

    small device design fixed

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -21,18 +21,40 @@ const supportList = [
   { title: "Support & resources ", link: "" },
 ];
 
-const support=[
-  {title:'Help Center',link:'/help'},
-  {title:'Get help with a safety issue',link:''},
-  {title:'AirCover',link:''},
-  {title:'Anti-discrimination',link:''},
-  {title:'Disability support',link:''},
-  {title:'Cancellation options',link:''},
-  {title:'Report neighborhood concern',link:''},
-]
+const support = [
+  { title: "Help Center", link: "/help" },
+  { title: "Get help with a safety issue", link: "" },
+  { title: "AirCover", link: "" },
+  { title: "Anti-discrimination", link: "" },
+  { title: "Disability support", link: "" },
+  { title: "Cancellation options", link: "" },
+  { title: "Report neighborhood concern", link: "" },
+];
+
+//small device
+const smallsupportList = [
+  { title: "English (US)", link: "" },
+  { title: "USD", link: "" },
+];
+
+
+
+const FooterMenuList = [
+  { path: "/giftcard", title: "GiftCard" },
+  { path: "/airbnbhome", title: "Home" },
+  { path: "/helpCenter", title: "Privecy" },
+];
+
+const smallMenuList = [
+  { path: "/giftcard", title: "Gift Card" },
+  { path: "/airbnbhome", title: "Airbnb Your Home" },
+  { path: "/helpCenter", title: "Hepl Center" },
+];
+
 const Footer = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
   const [open, setOpen] = useState(false);
+  const [isopen, setIsopen] = useState<boolean>(false);
   const showDrawer = () => {
     setOpen(true);
   };
@@ -41,90 +63,165 @@ const Footer = () => {
   };
 
   return (
-
     <div>
-     
-<div className="bg-primary bottom-0 w-full z-50 fixed shadow-sm border-t-[1px] h-[50px]  border-slate-200 ">
-    <div className="hidden md:block">
-      <div className={` flex flex-col lg:flex-row pt-2 justify-between items-center md:px-10 lg:px-20 text-sm font-thin text-black  ${pathname =='/rooms'? ' xl:px-[185px]' : ' lg:px-20' }`}>
-        <div className="flex justify-between items-center gap-2">
-          {quickList?.map((item, i) => {
-            return <p className="text-normal font-normal" key={i}>{item?.title}</p>;
-          })}
+      <div className="bg-primary bottom-0 w-full z-50 fixed shadow-sm border-t-[1px] h-auto  border-slate-200 mt-[120px] md:mt-0">
+        <div className="hidden md:block">
+          <div
+            className={` flex flex-col lg:flex-row pt-2 justify-between items-center md:px-10 lg:px-20 text-sm font-thin text-black  ${
+              pathname == "/rooms" ? " xl:px-[185px]" : " lg:px-20"
+            }`}
+          >
+            <div className="flex justify-between items-center gap-2">
+              {quickList?.map((item, i) => {
+                return (
+                  <p className="text-normal font-normal" key={i}>
+                    {item?.title}
+                  </p>
+                );
+              })}
+            </div>
+
+            <div className="flex justify-between items-center gap-2">
+              {supportList?.map((item, i) => {
+                return (
+                  <p
+                    className="flex justify-between gap-1 text-medium font-semibold hover:underline cursor-pointer"
+                    key={i}
+                  >
+                    {item?.title}{" "}
+                    {item?.title === "Support & resources " && (
+                      <FaChevronDown
+                        style={{ marginTop: "4px" }}
+                        onClick={showDrawer}
+                      />
+                    )}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
         </div>
 
-        <div className="flex justify-between items-center gap-2">
-          {supportList?.map((item, i) => {
-            return (
-              <p className="flex justify-between gap-1 text-medium font-semibold hover:underline cursor-pointer" key={i}>
-                {item?.title}{" "}
-                {item?.title === "Support & resources " && (
-                  <FaChevronDown
-                    style={{ marginTop: "4px" }}
-                    onClick={showDrawer}
-                  />
-                )}
-              </p>
-            );
-          })}
+        <div className="md:hidden">
+          {pathname === "/" || pathname === "/filterRoom" ? (
+            <SmallDeviceFooter setIsopen={setIsopen} isopen={isopen} />
+          ) : (
+            <>
+              <div className="px-2 bg-[#F7F7F7] pt-4 pb-4">
+                <div className="flex justify-start px-5 gap-4 ">
+                  {smallsupportList?.map((item, i) => {
+                    return (
+                      <p className=" text-sm font-normal " key={i}>
+                        {item?.title}{" "}
+                      </p>
+                    );
+                  })}
+                </div>
+
+                <div className=" px-5 gap-2 pt-1 ">
+                  <p className="text-sm font-light text-slate-900">
+                    © 2024 Airbnb, Inc.{" "}
+                  </p>
+                  <div className="flex justify-start gap-3 pt-1 ">
+                    {FooterMenuList?.map((item, i) => (
+                      <Link href={item?.path} key={i}>
+                        <p className="text-sm font-light text-slate-900">
+                          {item?.title}
+                        </p>
+                      </Link>
+                    ))}
+                  </div>
+                  <p className="text-sm font-light pt-1 text-slate-900">
+                    Your Privecy Choices.{" "}
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
         </div>
+
+        <Drawer
+          placement={"bottom"}
+          width={500}
+          onClose={onClose}
+          open={open}
+          // extra={
+          //   <Space>
+          //     <Button onClick={onClose}>Cancel</Button>
+          //     <Button type="primary" onClick={onClose}>
+          //       OK
+          //     </Button>
+          //   </Space>
+          // }
+        >
+          <div className="flex justify-evenly items-center flex-col md:flex-row  gap-4">
+            <div>
+              <p className="text-base font-semibold">Support</p>
+              {support?.map((item, i) => {
+                return (
+                  <Link href={item?.link} key={i}>
+                    <p
+                      onClick={() => setOpen(false)}
+                      className="text-base font-normal py-1 hover:underline decoration-1 cursor-pointer"
+                    >
+                      {item?.title}
+                    </p>
+                  </Link>
+                );
+              })}
+            </div>
+
+            <div>
+              <p className="text-base font-semibold">Hosting</p>
+              {support?.map((item, i) => {
+                return (
+                  <p
+                    className="text-base font-normal py-1 hover:underline decoration-1 cursor-pointer"
+                    key={i}
+                  >
+                    {item?.title}
+                  </p>
+                );
+              })}
+            </div>
+
+            <div>
+              <p className="text-base font-semibold">Airbnb</p>
+              {support?.map((item, i) => {
+                return (
+                  <p
+                    className="text-base font-normal py-1 hover:underline cursor-pointer decoration-1"
+                    key={i}
+                  >
+                    {item?.title}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        </Drawer>
+
+        {isopen && (
+          <div className="fixed top-[65%] right-2 bg-primary rounded-md shadow-md border-[1px] border-slate-200 z-50 p-4">
+            {smallMenuList.map((item, i) => (
+              <Link href={item?.path} key={i}>
+                <p
+                  key={i}
+                  className={`py-2 cursor-pointer ${
+                    item?.title === "Sign Up"
+                      ? "border-b-[1px] border-slate-400"
+                      : ""
+                  }`}
+                  onClick={() => setIsopen(false)}
+                >
+                  {item?.title}
+                </p>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
-    <div className="md:hidden">
-    <SmallDeviceFooter/>
-    </div>
-      <Drawer        
-        placement={"bottom"}
-        width={500}
-        onClose={onClose}
-        open={open}
-        // extra={
-        //   <Space>
-        //     <Button onClick={onClose}>Cancel</Button>
-        //     <Button type="primary" onClick={onClose}>
-        //       OK
-        //     </Button>
-        //   </Space>
-        // }
-      >
-       <div className="flex justify-evenly items-center flex-col md:flex-row  gap-4">
-        <div>
-          <p className="text-base font-semibold">Support</p>
-          {
-            support?.map((item,i)=>{
-              return <Link href={item?.link} key={i}>
-              <p onClick={()=>setOpen(false)} className="text-base font-normal py-1 hover:underline decoration-1 cursor-pointer">{item?.title}</p>
-
-              </Link>
-            })
-          }
-        </div>
-
-        <div>
-        <p className="text-base font-semibold">Hosting</p>
-          {
-            support?.map((item,i)=>{
-              return <p className="text-base font-normal py-1 hover:underline decoration-1 cursor-pointer" key={i}>{item?.title}</p>
-            })
-          }
-        </div>
-
-        <div>
-        <p className="text-base font-semibold">Airbnb</p>
-          {
-            support?.map((item,i)=>{
-              return <p className="text-base font-normal py-1 hover:underline cursor-pointer decoration-1" key={i}>{item?.title}</p>
-            })
-          }
-        </div>
-
-       </div>
-      </Drawer>
-
-     
-    </div>
-    </div>
-    
   );
 };
 
--- components/layout/smallDeviceFooter.tsx
+++ components/layout/smallDeviceFooter.tsx
@@ -1,77 +1,143 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { IoSearchOutline } from "react-icons/io5";
 import { MdFavoriteBorder } from "react-icons/md";
 import CommonModal from "../common/commonmodal";
 import CommonLogin from "../login/page";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import Link from "next/link";
+import { TiThMenu } from "react-icons/ti";
+import { setSearchModal } from "@/redux/reducer/authReducer";
 
 interface AuthDataType {
   token: string; // Assuming token is a string
-  firstName:string;
+  firstName: string;
   // Add other properties as needed
 }
 
+interface FooterProps {
+  isopen: boolean;
+  setIsopen: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const SmallDeviceFooter = () => {
-  const authData = useSelector((state: RootState) => state.auth?.authData) as AuthDataType;
-     //modal open part
-     const [isModalOpen, setIsModalOpen] = useState(false);
-     const _handleCancel = () => {
-       setIsModalOpen(false);
-     };
-  const[active,setActive]=useState('Explore')
+const SmallDeviceFooter = ({ setIsopen }: FooterProps) => {
+  const dispatch = useDispatch();
+  const authData = useSelector(
+    (state: RootState) => state.auth?.authData
+  ) as AuthDataType;
+  //modal open part
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const _handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const [active, setActive] = useState("Explore");
   return (
-    <div className="flex justify-evenly items-center pt-2  mx-auto">
-      <div onClick={()=>setActive('Explore')} className={`${active === 'Explore' ? 'text-secondary font-semibold' : 'font-light'}`}>
-        <div className="flex justify-center items-center">
-          <IoSearchOutline
-            style={{ fontWeight: "bold", height: "20px", width: "20px" }}
-          />
+    <div>
+      <div className="flex justify-center gap-10 items-center pt-2  mx-auto">
+        <div
+          onClick={() => {
+            setActive("Explore");
+            dispatch(setSearchModal(true));
+          }}
+          className={`${
+            active === "Explore" ? "text-secondary font-semibold" : "font-light"
+          }`}
+        >
+          <div className="flex justify-center items-center">
+            <IoSearchOutline
+              style={{ fontWeight: "bold", height: "20px", width: "20px" }}
+            />
+          </div>
+          <p className="text-xs ">Explore</p>
         </div>
-        <p className="text-xs ">Explore</p>
-      </div>
-      <div onClick={()=>setActive('Whistle')} className={`${active === 'Whistle' ? 'text-secondary font-semibold' : 'font-light'}`}>
-        <div className="flex justify-center items-center">
-          <MdFavoriteBorder />
+        <div
+          onClick={() => setActive("Whistle")}
+          className={`${
+            active === "Whistle" ? "text-secondary font-semibold" : "font-light"
+          }`}
+        >
+          <div className="flex justify-center items-center">
+            <MdFavoriteBorder />
+          </div>
+          <p className="text-xs ">Whistle</p>
         </div>
-        <p className="text-xs ">Whistle</p>
-      </div>
 
-     {authData?.token ?  <>
-     <Link href={'/profile'}>
-    
-       <div onClick={()=>{setActive('Profile');}} className={`${active === 'Profile' ? 'text-secondary font-semibold' : 'font-light'}`}>
-        <div className="flex justify-center items-center">
-          <FaUserCircle
-            style={{ fontWeight: "bold", height: "20px", width: "20px" }}
-          />
+        {authData?.token ? (
+          <>
+            <Link href={"/profile"}>
+              <div
+                onClick={() => {
+                  setActive("Profile");
+                }}
+                className={`${
+                  active === "Profile"
+                    ? "text-secondary font-semibold"
+                    : "font-light"
+                }`}
+              >
+                <div className="flex justify-center items-center">
+                  <FaUserCircle
+                    style={{
+                      fontWeight: "bold",
+                      height: "20px",
+                      width: "20px",
+                    }}
+                  />
+                </div>
+                <p className="text-xs ">Profile</p>
+              </div>
+            </Link>
+          </>
+        ) : (
+          <div
+            onClick={() => {
+              setActive("Log In");
+              setIsModalOpen(true);
+            }}
+            className={`${
+              active === "Log In"
+                ? "text-secondary font-semibold"
+                : "font-light"
+            }`}
+          >
+            <div className="flex justify-center items-center">
+              <FaUserCircle
+                style={{ fontWeight: "bold", height: "20px", width: "20px" }}
+              />
+            </div>
+            <p className="text-xs ">Log In</p>
+          </div>
+        )}
+
+        <div
+          onClick={() => {
+            setActive("Menu");
+            setIsopen((prevIsOpen) => !prevIsOpen);
+          }}
+          className={`${
+            active === "Menu" ? "text-secondary font-semibold" : "font-light"
+          }`}
+        >
+          <div className="flex justify-center items-center">
+            <TiThMenu
+              style={{ fontWeight: "bold", height: "20px", width: "20px" }}
+            />
+          </div>
+          <p className="text-xs ">Menu</p>
         </div>
-        <p className="text-xs ">Profile</p>
-      </div>
-      </Link>
-      </>  :<div onClick={()=>{setActive('Log In'); setIsModalOpen(true);}} className={`${active === 'Log In' ? 'text-secondary font-semibold' : 'font-light'}`}>
-        <div className="flex justify-center items-center">
-          <FaUserCircle
-            style={{ fontWeight: "bold", height: "20px", width: "20px" }}
-          />
-        </div>
-        <p className="text-xs ">Log In</p>
-       </div>
-    }
-      <CommonModal
+
+        <CommonModal
           open={isModalOpen}
           setIsModalOpen={setIsModalOpen}
           title={`Log in or sign up`}
           onCancel={_handleCancel}
           width={"500px"}
         >
-          <CommonLogin setIsModalOpen={setIsModalOpen}/>
-         
+          <CommonLogin setIsModalOpen={setIsModalOpen} />
         </CommonModal>
+      </div>
     </div>
   );
 };
